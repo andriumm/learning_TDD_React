448b256e61a3e5603bbc54e12e090afe5d133cb8
Andrea Mestre <amestre89@gmail.com>
2021-04-06 21:34:50 +0200

    first test

---
+++ src/__tests__/example_globals.test.js
@@ -0,0 +1,12 @@
+describe("this is for grouping test cases", () => {
+	it("description", () => {
+		expect(true).toBe(true);
+	});
+
+	it("description2", () => {
+		expect(true).toBe(true);
+	});
+	it("description3", () => {
+		expect(true).toBe(true);
+	});
+});
