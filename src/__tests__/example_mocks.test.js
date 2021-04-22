331ad591f094a1aa4d315f02b655f659a8d2d602
Andrea Mestre <amestre89@gmail.com>
2021-04-22 11:48:53 +0200

    mock examples

--- src/__tests__/example_mocks.test.js
+++ src/__tests__/example_mocks.test.js
@@ -1,3 +1,8 @@
+import { storage } from "../lib/storage";
+import { saveUsername, getUsername } from "../user";
+
+jest.mock("../lib/storage");
+
 test("first example", () => {
 	const myMock = jest
 		.fn()
@@ -14,4 +19,27 @@ test("first example", () => {
 	expect(result1).toBe(true);
 	expect(result2).toBe("hello world");
 	expect(result3).toBe(5);
+});
+
+test("second example", () => {
+	const username = "andrea mestre";
+	saveUsername(username);
+	expect(storage.save).toHaveBeenCalledTimes(1);
+	expect(storage.save).toHaveBeenLastCalledWith({
+		key: "username",
+		value: username,
+	});
+});
+
+test("third example", () => {
+	const username = "andrea mestre";
+	storage.get.mockReturnValueOnce(username);
+
+	const result = getUsername();
+
+	expect(result).toBe(username);
+	expect(storage.get).toHaveBeenCalledTimes(1);
+	expect(storage.get).toHaveBeenLastCalledWith({
+		key: "username",
+	});
 });
